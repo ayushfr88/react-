6da419cc54f675b3835398031232b9b3b835d2ed
ayushfr88 <ayushpatel9301221186@gmail.com>
2025-07-24 20:41:42 +0530

    context-api-discussion

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import { useState } from "react"
+import UserContext from "./context/UserContext"
+import UserContextProvider from "./context/UserContextProvider"
+import "./App.css"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1></h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
